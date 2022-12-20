25efb04dc2e0c69494465fd9f8ae7d10529ba5c2
HP <meharhn1012515@gmail.com>
2022-12-20 07:35:49 -0700

    message

---
+++ car.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+main(){
+cout<<"                                            *****************************************                           "<<endl;
+cout<<"                                          *                                          *                                   "<<endl;
+cout<<"                                         *                                            *                                    "<<endl;
+cout<<"                                        *                                              *                                    "<<endl;
+cout<<"                                       *                                                *                                    "<<endl;
+cout<<"           ****************************                                                  *                               "<<endl;
+cout<<"         *                                                                                 *                   "<<endl;
+cout<<"       *                                                                                     *              "<<endl;
+cout<<"     *                                                                                         ******************             "<<endl;
+cout<<"   *                                  * ******************************                                            *     "<<endl;
+cout<<"  *                                    *                                                                            *                 "<<endl;
+cout<<"  *                                     *    L A M B O R G I N E                                                     *        "<<endl;
+cout<<"  *                                      *                                                                            *                           "<<endl;
+cout<<"  *              *  *  *  *               * ****************                    *  *  *  *                            *                "<<endl;
+cout<<"   *          *             *                                                 *             *                        *                 "<<endl;
+cout<<"    *        *               *                                               *               *                     *                      "<<endl;
+cout<<"     *      *                 *                                             *                 *                  *                  "<<endl;
+cout<<"       ******                 ***********************************************                 ******************                       "<<endl;
+cout<<"            *                 *                                             *                 *                               "<<endl;
+cout<<"             *               *                                               *               *                          "<<endl;
+cout<<"              *             *                                                 *             *                            "<<endl;
+cout<<"                * * * * * *                                                     * * * * * *                                      "<<endl;
+
+}
